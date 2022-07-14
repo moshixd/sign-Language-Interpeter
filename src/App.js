c977379683c942015bc7784db67a12a93f248662
Salah Abdinoor <12002329-SalahAbdinoor@users.noreply.gitlab.com>
2022-07-14 15:30:23 +0200

    added diagram/todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Rest from "./components/Rest";
-import Users from "./components/Users";
 
 function App() {
   return (
     <div className="App">
       <Rest />
-      <Users />
     </div>
   );
 }
