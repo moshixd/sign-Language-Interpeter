2b46fc5d25e496a2fe53275dc9202c1621558a36
Salah Abdinoor <12002329-SalahAbdinoor@users.noreply.gitlab.com>
2022-07-14 14:13:50 +0200

    adding translation resources

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Rest from "./components/Rest";
+import Users from "./components/Users";
 
 function App() {
   return (
     <div className="App">
       <Rest />
+      <Users />
     </div>
   );
 }
